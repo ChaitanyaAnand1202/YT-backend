4000c9eaaa3c3a8e383102554fa47810f984e2f8
CHAITANYA ANAND <anandchaitanya12@gmail.com>
2024-04-26 14:44:51 +0530

    added async Handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err));
+  }
+}
+
+export {asyncHandler};
+
+
+
+
+
+
+// using try-catch block
+
+// const asyncHandler = (func) => async(req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch(error){
+//     res.status(err.code || 500).json({
+//       succes: false,
+//       message: err.message
+//     })
+//   }
+// }
+
+// how to get to the above notation?
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {
+//   () => {}
+// }
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async() => {}
