1a56ed3b40f94a12f98318bdd6018168190fcc34
CHAITANYA ANAND <anandchaitanya12@gmail.com>
2024-05-05 19:41:52 +0530

    import dotenv for env variable access

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,23 @@
 import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
+
+// cloudinary.config({ 
+//   cloud_name: 'anand-dev-12', 
+//   api_key: '282728916793433', 
+//   api_secret: 'sllpYBAZ4HAdcOl6gJNOeXAKZvI' 
+// });
 
 // how upload works - file is uploaded by the user and is temporarily stored at a local server using multer middleware
 // from the temporary local storage (public/temp) it is uploaded to cloudinary using this utility
@@ -24,8 +36,14 @@ const uploadOnCloudinary = async (localFilePath) => {
     })
 
     // now file is uploaded successfully, return the response
-    console.log("File uploaded to cloudinary successfully", response.url);
+    // console.log("File uploaded to cloudinary successfully", response.url);
+    
+    
+    // remove the files after successfully uploading
+    fs.unlinkSync(localFilePath);
+    // return response
     return response;
+    
   } catch (error) {
     // if there is an error during upload, it means file may be corrupted, so the file should be removed from the temporary local storage
     // file system is used to unlink(delete) the file synchronously -> file should be deleted first, only then proceed
